e7018b4c5f69dfb3dac7f4282b286fadd156cfdf
Tung Bui <tung.bquang@gmail.com>
2024-05-18 22:46:22 +0700

    Improve codebase.py

--- env/python/execute_python.sh
+++ env/python/execute_python.sh
@@ -3,33 +3,18 @@
 # Import common script
 source "../common/common.sh"
 
-# Usage: ./execute_python.sh src/demo.py
-# Function to display usage information
-usage() {
-  echo "Usage: $0 <script_path>"
-  echo "Example: $0 src/test.py"
-  exit 1
-}
-
+LANGUAGE="python"
 script_path=$1
+check_input $0 $script_path
+CMD="python /tmp/$script_path"
 
-# Load from common
+# Load from common scipt
 yaml_file=$CONFIG_FILE_PATH
-
 check_file_exists $yaml_file
 
 # Read and parse the YAML file
-python_image=$(yq e '.python.image' $yaml_file)
-container_name=$(yq e '.python.default_container_name' $yaml_file)
-echo "python_image: $python_image"
-echo "container_name: $container_name"
-
-# Check if the script name is provided
-if [ -z "$script_path" ]; then
-  echo "Error: No script name provided."
-  usage
-fi
+image=$(yq e ".$LANGUAGE.image" $yaml_file)
+container_name=$(yq e ".$LANGUAGE.default_container_name" $yaml_file)
 
 # Command to execute
-CMD="python /tmp/$script_path"
-execute_in_docker "$python_image" "$container_name" "$CMD"
+execute_in_docker "$image" "$container_name" "$CMD"
